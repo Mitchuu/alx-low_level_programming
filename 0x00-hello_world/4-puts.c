ea02d38a1f626e618a68aee5071b1a31772c1fc7
Mitchuu <anyangomitchelle7@yahoo.com>
2022-07-03 01:44:23 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Returns: 0 after printing the fucntion
+ * puts
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,/n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -8,3 +8,4 @@ Compiler
 Assembler
 Name
 Hello, printf
+Hello, puts
