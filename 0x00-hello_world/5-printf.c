11d49b7667cc6150bc92a56333ab23d1436f9925
Mitchuu <anyangomitchelle7@yahoo.com>
2022-07-03 10:48:30 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	printf("with proper grammer, but out the outcome is a piece of art,\n");
+	printf("with proper grammar, but out the outcome is a piece of art,\n");
 	return (0);
 }
